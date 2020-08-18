e234d1d1ffbce875edae96b3924b245547566322
liuq <liuqing_210@163.com>
2020-08-18 19:22:23 +0800

    修改信息并提交

--- src/main/java/com/itheima/pojo/User.java
+++ src/main/java/com/itheima/pojo/User.java
@@ -1,12 +1,15 @@
 package com.itheima.pojo;
 
+import java.io.Serializable;
+
 /**
  * Package: com.itheima.pojo
  *
  * @Author: liuqing
  * @Date: 2020/8/18 18:12
  */
-public class User {
+public class User implements Serializable {
     private String name;
     private Integer age;
+    private String sex;
 }
