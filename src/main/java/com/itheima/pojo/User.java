23f2c42c461453b963a4d057cf30b3795a29e676
liuq <liuqing_210@163.com>
2020-08-18 18:18:05 +0800

    添加文件

---
+++ src/main/java/com/itheima/pojo/User.java
@@ -0,0 +1,12 @@
+package com.itheima.pojo;
+
+/**
+ * Package: com.itheima.pojo
+ *
+ * @Author: liuqing
+ * @Date: 2020/8/18 18:12
+ */
+public class User {
+    private String name;
+    private Integer age;
+}
